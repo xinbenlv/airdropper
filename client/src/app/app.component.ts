b255891a422c6bf9d7381a4134b4be7d43a5dca8
Zainan Victor Zhou <zzn@google.com>
2018-07-05 14:54:56 -0700

    Update

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -15,7 +15,7 @@ export class MyApp {
   constructor(
     platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,
     private translate: TranslateService) {
-    LoopBackConfig.setBaseURL(`http://localhost:3000`);
+    //LoopBackConfig.setBaseURL(``);
     LoopBackConfig.setApiVersion('api');
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
